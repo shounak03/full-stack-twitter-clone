cfa33024fe73df16ac69c4a1a256dc1fe8954d8f
shounak03 <shounakmis.3@gmail.com>
2024-09-12 13:20:09 +0530

    ui completed

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -1,23 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../../public/logo-twitter.png'
 import { Link } from 'react-router-dom'
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    emailOrUsername: "",
+    password: ""
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  }
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
       <div className='flex items-center justify-evenly w-[80%]'>
-        <div>
+        <div className='hidden lg:flex '>
           <img src={logo} alt="" className='ml-5' width={'200px'}/>
         </div>
         <div>
+        <img src={logo} className='w-24 lg:hidden fill-white' alt="" />
           <div className='my-5 w-full'>
-            <h1 className='font-bold text-6xl'> Happening Now</h1>
+            <h1 className='font-bold text-6xl'>Happening Now</h1>
           </div>
-          <h1 className='mt-5 mb-4 text-4xl font-bold w-full'>Login</h1>
-          <form action="" className='flex flex-col w-[80%]'>
-            <input type="text" placeholder='Username or Email' className='border-gray-800 border outline-blue-500 px-3 py-1 rounded-full my-2 font-semibold'  />
-            <input type="password" placeholder='Password' className='border-gray-800 border outline-blue-500 px-3 py-1 rounded-full my-2 font-semibold'  />
+          <h1 className='mt-5 mb-4 text-3xl font-bold w-full'>Login</h1>
+          <form action="" className='flex flex-col w-[60%]' onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              name="emailOrUsername"
+              value={formData.emailOrUsername}
+              onChange={handleInputChange}
+              placeholder='Username or Email' 
+              className='border-gray-800 border outline-blue-500 px-3 py-1 rounded-full my-2 font-semibold'  
+            />
+            <input 
+              type="password" 
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              placeholder='Password' 
+              className='border-gray-800 border outline-blue-500 px-3 py-1 rounded-full my-2 font-semibold'  
+            />
             <button className='appearance-none bg-black border-2 mt-2 border-[#1A1A1A] rounded-full box-border text-white cursor-pointer inline-block font-sans font-semibold text-[16px] w-[50%] h-[40px]'>
-                        Login</button>
+              Login
+            </button>
             <div className='my-4'>
               <h1>
                 Don't have an account? <Link to="/register" className='font-bold text-gray-1000 underline'>Register</Link>
--- frontend/src/components/Auth/SignUp.jsx
+++ frontend/src/components/Auth/SignUp.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import logo from '../../../public/logo-twitter.png'
+import { Link } from 'react-router-dom'
 const SignUp = () => {
+
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    fullname: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+  const isError = false;
   return (
-    <div>SignUp</div>
+    <div className='w-screen h-screen flex items-center justify-center'>
+      <div className='flex items-center justify-evenly w-[80%]'>
+        <div className=' hidden lg:flex '>
+          <img src={logo} alt="" className='ml-5 ' width={'200px'} />
+        </div>
+        <div>
+          <div className='my-5 w-full'>
+          <img src={logo} className='w-24 lg:hidden fill-white' alt="" />
+            <h1 className='font-bold text-6xl'>Join Today</h1>
+          </div>
+          <h1 className='mt-5 mb-4 text-4xl font-bold w-full'>SignUp</h1>
+          <form action="" className='flex flex-col w-[80%]' onSubmit={handleSubmit}>
+            
+            <input type="text" placeholder='Email' className='border-gray-800 border outline-blue-500 px-3 py-1 rounded-full my-2 font-semibold'
+              name='email'
+              onChange={handleInputChange}
+              value={formData.email} />
+            <input type="text" placeholder='Username' className='border-gray-800 border outline-blue-500 px-3 py-1 rounded-full my-2 font-semibold' 
+              name='username'
+							onChange={handleInputChange}
+							value={formData.username}/>
+            <input type="text" placeholder='Fullname' className='border-gray-800 border outline-blue-500 px-3 py-1 rounded-full my-2 font-semibold' 
+              name='fullname'
+							onChange={handleInputChange}
+							value={formData.fullname}/>
+            <input type="password" placeholder='Password' className='border-gray-800 border outline-blue-500 px-3 py-1 rounded-full my-2 font-semibold' 
+              name='password'
+							onChange={handleInputChange}
+							value={formData.password}/>
+            <button className='appearance-none bg-black border-2 mt-2 border-[#1A1A1A] rounded-full box-border text-white cursor-pointer inline-block font-sans font-semibold text-[16px] w-[50%] h-[40px]'>
+              SignUp</button>
+            {isError && <p className='text-red-500 mt-3'>Something went wrong</p>}
+            <div className='my-4'>
+              <h1>
+                Already have an account? <Link to="/login" className='font-bold text-gray-1000 underline'>Login</Link>
+              </h1>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
